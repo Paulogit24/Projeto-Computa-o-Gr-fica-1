e5d8e02e90bc6e7e560181d34d438689581cf513
Paulogit24 <40240257@esmad.ipp.pt>
2025-10-29 16:45:51 +0000

    menu (less incomplete)

--- js/index.js
+++ js/index.js
@@ -7,20 +7,31 @@ const startButton = {
     width: 175,
     height: 100,
     text: 'Start',
+    textColor: '#ffffff',
+    color: '#44ff00ff',
+    border: '#054d04ff',
+    borderRadius: 20
 }
 function createButton(button) {
-    // Desenhar o retângulo do botão
-    ctx.fillStyle = '#44ff00ff'; // Cor do botão
-    ctx.fillRect(button.x, button.y, button.width, button.height);
-
-    // Desenhar o texto do botão
-    ctx.fillStyle = '#ffffffff'; // Cor do texto
+    //retângulo do botão
+    ctx.roundRect(button.x, button.y, button.width, button.height, button.borderRadius);
+    ctx.fillStyle = button.color; // Cor do botão
+    ctx.fill();
+    //texto do botão
+    ctx.fillStyle = button.textColor; // Cor do texto
     ctx.font = '30px Arial';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillText(button.text, button.x + button.width / 2, button.y + button.height / 2);
+    //borda do botão
+    ctx.strokeStyle = button.border; // Cor da borda
+    ctx.lineWidth = 4;
+    ctx.stroke();
 }
+createButton(startButton);
 
-// Desenhar um retângulo verde (representando grama)
-ctx.fillStyle = '#2600fc43'; // Cor verde clara
-ctx.fillRect(250, 300, 175, 100); // x, y, largura, altura
+// go get the text here https://www.w3schools.com/jsref/canvas_filltext.asp
+
+//ctx.font = "20px Georgia";
+ctx.fillStyle = '#000000'; // Black color, you can change this to any color
+ctx.fillText("nome do jogo", 340, 50, 700);
